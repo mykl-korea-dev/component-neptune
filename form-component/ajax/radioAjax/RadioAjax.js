9e49d7c351dd5429226174ba6cac6d0011babd4b
euNung <pandamon24@gmail.com>
2023-01-12 12:09:47 +0900

    23.01.12

--- form-component/ajax/radioAjax/RadioAjax.js
+++ form-component/ajax/radioAjax/RadioAjax.js
@@ -2,18 +2,19 @@ import Component from "../../../basic/Component.js";
 
 export default class RadioAjax extends Component {
     setElements() {
-        this.name = this.$element.querySelector('input').getAttribute('name');
+        this.name = this.$element.querySelector('input').getAttribute('name') || "";
     }
 
     setTemplate() {
         if(Array.isArray(this.$data) || this.$data.data) {
             const {data: $data = this.$data, options = {}} = this.$data;
+            let name = "";
             return $data.map(data => {
-                const {id = "id", value = "value", checked = "checked", trueChecked = 'Y'} = options;
+                const {id = "id", value = "value", checked = "checked", trueChecked = 'Y', name = "name"} = options;
                 return `
                 <div class="mykl-radio">
-                    <input class="radio-input" type="radio" name="${this.name}" id="${this.name}-${data[id]}" value="${data[id]}"  ${data[checked] === trueChecked ? "checked" : ''}>
-                    <label class="radio-label" for="${this.name}-${data[id]}">${data[value]}</label>
+                    <input class="radio-input" type="radio" name="${this.name || data[name]}" id="${this.name || data[name]}-${data[id]}" value="${data[id]}"  ${data[checked] === trueChecked ? "checked" : ''}>
+                    <label class="radio-label" for="${this.name || data[name]}-${data[id]}">${data[value]}</label>
                 </div>
             `
             }).join('');

8c8acb6579e43b1064ce4b25706da72b56625a48
euNung <pandamon24@gmail.com>
2023-02-21 16:03:42 +0900

    feat: formAjax - radio,checkbox,select에서 동작 오류 수정

--- form-component/ajax/formAjax.js
+++ form-component/ajax/formAjax.js
@@ -8,27 +8,33 @@ export default class FormAjax extends Component {
             e.preventDefault();
             const objs = {};
             [...this.$element.querySelectorAll('[name]')].forEach(el => {
-                if(['radio', 'checkbox', 'select'].includes(el.getAttribute('type'))) {
-                    if(el.checked) {
-                        objs[el.getAttribute('name')] = el.value;
-                    } else if(this.$data.default?.[el.getAttribute('name')]) {
-                        objs[el.getAttribute('name')] = this.$data.default?.[el.getAttribute('name')]
-                    }
-                } else {
-                    if(this.$data.list?.[el.getAttribute('name')]) {
-                        if(el.value) {
-                            if(/\[]/.test(this.$data.list[el.getAttribute('name')])) {
+                const isFormEl = ['radio', 'checkbox', 'select'].includes(el.getAttribute('type'));
+                const isConnectText = this.$data.list?.[el.getAttribute('name')];
+                const isDefaultText = this.$data.default?.[el.getAttribute('name')];
+                // if(['radio', 'checkbox', 'select'].includes(el.getAttribute('type'))) {
+                //     if(el.checked) {
+                //         objs[el.getAttribute('name')] = el.value;
+                //     } else if(this.$data.default?.[el.getAttribute('name')]) {
+                //         objs[el.getAttribute('name')] = this.$data.default?.[el.getAttribute('name')]
+                //     }
+                // } else {
+                    if(isConnectText || (isFormEl && el.checked && isConnectText)) {
+                        const isValue = isFormEl ? el.checked : el.value;
+                        if(isValue) {
+                            if(/\[]/.test(isConnectText)) {
                                 objs[el.getAttribute('name')] = objs[el.getAttribute('name')]
                                     ? [...objs[el.getAttribute('name')], el.value] : [el.value];
                             } else {
                                 objs[el.getAttribute('name')] = objs[el.getAttribute('name')]
-                                    ? objs[el.getAttribute('name')] + this.$data.list[el.getAttribute('name')] + el.value : el.value;
+                                    ? objs[el.getAttribute('name')] + isConnectText + el.value : el.value;
                             }
                         }
+                    } else if (isDefaultText) {
+                        objs[el.getAttribute('name')] = isDefaultText;
                     } else {
                         objs[el.getAttribute('name')] = objs[el.getAttribute('name')] ? [...objs[el.getAttribute('name')], el.value] : el.value
                     }
-                }
+                // }
             });
 
             this.$data.callback(objs);

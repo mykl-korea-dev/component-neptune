3d6e48931593a60011bb61498fbbe3bb0ef5396a
devfrank9 <devfrank9@gmail.com>
2023-04-13 12:25:36 +0900

    progress: 다중 체크 컴포넌트 작업중

--- form-component/select-multi-check/Select-multi-check.js
+++ form-component/select-multi-check/Select-multi-check.js
@@ -3,8 +3,8 @@ import Component from "../../basic/Component.js";
 export default class SelectMultiCheck extends Component {
   setElements() {
     this.list = [];
-    this.selectDom = document.querySelectorAll("select");
-    [...this.selectDom].map((el) => {
+
+    [...this.$element.querySelectorAll("select")].map((el) => {
       this.list.push({
         id: el.id,
         option: el.options,
@@ -12,6 +12,57 @@ export default class SelectMultiCheck extends Component {
         name: el.name,
       });
     });
+  }
+
+  selectClickEvent(optionDom) {
+    // TODO : 옵션 선택시 컨테이너에 인풋 넣기
+    const checkDom = optionDom.querySelector("input");
+
+    if (checkDom.checked) {
+      checkDom.checked = false;
+    } else {
+      checkDom.checked = true;
+    }
+  }
+
+  setOptionsTemplate(triggerElement) {
+    [...this.$element.querySelectorAll(".select-trigger")]
+      .filter((dom) => dom.id !== triggerElement.id)
+      .map((el) => el.classList.remove("on"));
+
+    const selectOptionsDom = this.$element.querySelector(".select-options");
+    if (triggerElement.classList.contains("on")) {
+      triggerElement.classList.remove("on");
+      selectOptionsDom.style.display = "none";
+    } else {
+      triggerElement.classList.add("on");
+      selectOptionsDom.style.display = "";
+    }
+
+    const select = this.list.find((el) => el.id === triggerElement.id);
+    selectOptionsDom.innerHTML = `
+            ${[...select.option]
+              .map(
+                (option) =>
+                  `
+                    <div class="option-container">
+                        <input type="checkbox" class="mykl-check" id="${option.value}" name="${select.name}" value="${option.value}">
+                        <label for="${option.value}">${option.innerText}</label>
+                    </div>
+                `
+              )
+              .join("")}
+        `;
+
+    return [...this.$element.querySelectorAll(".option-container")].map(
+      (option) => (option.onclick = (e) => this.selectClickEvent(e.target))
+    );
+  }
+
+  setEvents() {
+    return [...this.$element.querySelectorAll(".select-trigger")].map(
+      (trigger) => (trigger.onclick = (e) => this.setOptionsTemplate(e.target))
+    );
   }
 
   setTemplate(element) {
@@ -29,56 +80,7 @@ export default class SelectMultiCheck extends Component {
         `;
   }
 
-  setOptionsTemplate(triggerElement) {
-    const resetDom = document.querySelectorAll(".select-trigger");
-    [...resetDom]
-      .filter((dom) => dom.id !== triggerElement.id)
-      .map((el) => el.classList.remove("on"));
-
-    const select = this.list.find((el) => el.id === triggerElement.id);
-    const selectOptionsDom = document.querySelector(".select-options");
-
-    if (triggerElement.classList.contains("on")) {
-      triggerElement.classList.remove("on");
-      selectOptionsDom.style.display = "none";
-    } else {
-      triggerElement.classList.add("on");
-      selectOptionsDom.style.display = "";
-    }
-
-    const template = `
-            ${[...select.option]
-              .map(
-                (option) =>
-                  `
-                    <div>
-                        <input type="checkbox" class="mykl-check" id="${option.value}" name="${select.name}" value="${option.value}">
-                        <label for="${option.value}">${option.innerText}</label>
-                    </div>
-                `
-              )
-              .join("")}
-        `;
-
-    selectOptionsDom.innerHTML = template;
-  }
-
-  setEvents() {
-    const triggers = this.$element.querySelectorAll(".select-trigger");
-    [...triggers].map(
-      (trigger) => (trigger.onclick = (e) => this.setOptionsTemplate(e.target))
-    );
-
-    const check = this.$element.querySelectorAll(".mykl-check");
-    [...check].map(
-      (el) =>
-        (el.onchange = (e) => {
-          console.log(e.target);
-        })
-    );
-  }
-
   render() {
-    this.$element.innerHTML = this.setTemplate(this.list);
+    return (this.$element.innerHTML = this.setTemplate(this.list));
   }
 }
